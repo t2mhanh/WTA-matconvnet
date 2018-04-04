e1c340e3f5e80bcc26cebdf7f67cda1ddd8f15df
t2mhanh <t2mhanh@gmail.com>
2018-04-04 13:43:09 +0100

    Add files via upload

---
+++ matlab/+dagnn/SpatialSparsity.m
@@ -0,0 +1,46 @@
+classdef SpatialSparsity < dagnn.ElementWise
+    % 1st version: 8th Feb
+%   properties
+%     rate = 0.5
+%     frozen = false
+%   end
+
+  properties (Transient)
+    mask
+  end
+
+  methods
+    function outputs = forward(obj, inputs, params)
+      if strcmp(obj.net.mode, 'test')
+        outputs = inputs ;
+        return ;
+      end
+%       if obj.frozen & ~isempty(obj.mask)
+%         outputs{1} = vl_nnSpatialSparsity(inputs{1}, 'mask', obj.mask) ;
+%       else
+        [outputs{1}, obj.mask] = vl_nnSpatialSparsity(inputs{1}) ;
+%       end
+    end
+
+    function [derInputs, derParams] = backward(obj, inputs, params, derOutputs)
+      if strcmp(obj.net.mode, 'test')
+        derInputs = derOutputs ;
+        derParams = {} ;
+        return ;
+      end
+      derInputs{1} = vl_nnSpatialSparsity(inputs{1}, derOutputs{1}, 'mask', obj.mask) ;
+      derParams = {} ;
+    end
+
+    % ---------------------------------------------------------------------
+    function obj = SpatialSparsity(varargin)
+      obj.load(varargin{:}) ;
+    end
+
+    function obj = reset(obj)
+      reset@dagnn.ElementWise(obj) ;
+      obj.mask = [] ;
+%       obj.frozen = false ;
+    end
+  end
+end
